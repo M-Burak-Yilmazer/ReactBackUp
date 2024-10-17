64afdf28b11cabd18c0d317314750a522171aabb
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-10-17 15:07:02 +0300

    product css completed

--- product_list_project1/src/App.js
+++ product_list_project1/src/App.js
@@ -1,15 +1,16 @@
-import Header from "./components/header/Header";
+
 import ProductList from "./components/products/ProductList";
 
 import { categories } from "./helper/data";
 import { products } from "./helper/data";
 import "./App.scss";
+import HeaderHamburger from "./components/headerHamburger/HeaderHamburger";
 
 function App() {
   console.log(products, categories);
   return (
     <div>
-      <Header categories={categories} text="ProductList" />
+      <HeaderHamburger categories={categories} text="ProductList" />
       <ProductList products={products} />
     </div>
   );
