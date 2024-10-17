71f28ded2b04394a60bc6d6a5bed9581085c7e53
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-10-17 22:55:06 +0300

    entering the states

--- react_events/src/1-events/Event.jsx
+++ react_events/src/1-events/Event.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Event = () => {
+  const handleTikla = () => alert("tiklandi");
+  const handleSil = function (msg) {
+    console.log("silindi");
+    console.log(msg);
+  };
 
-    const handleTikla =()=>
-        alert("tiklandi")
+  let message = "Fs15";
+  const handleChange = () => {
+     (message = "ch2024");
+    console.log(message);
+  };
 
   return (
     <div>
-    <h1>Events</h1>
-    <button onClick={handleTikla}> Tikla</button>
-      
+      <h1>Events</h1>
+      <button onClick={handleTikla}> Tikla</button>
+      <button onClick={() => handleSil(message)}>sil</button>
+      <button onClick={handleChange}>değiştir</button>
     </div>
-  )
-}
+  );
+};
 
-export default Event
+export default Event;
