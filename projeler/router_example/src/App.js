b2c9a40c9edc1bb3a82a1409cb0e29ff3e87e204
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-11-22 13:18:16 +0300

    react router start

---
+++ projeler/router_example/src/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/inst" element={<Instructors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
