41ac4611c9477821bc6c64969284b8a11718441c
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2025-01-05 18:48:02 +0300

    tailwind css

---
+++ tailwind/rc13_tailwind/src/App.js
@@ -0,0 +1,27 @@
+import FlexAndResponsive from "./components/FlexAndResponsive";
+import HoverFocus from "./components/HoverFocus";
+import Navbar from "./components/Navbar";
+import SpacingAndSizing from "./components/SpacingAndSizing";
+import Typografy from "./components/Typografy";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+
+    <div className="App">
+       <SpacingAndSizing /> 
+    {/* //   <Navbar /> */}
+      {/* <h1 className="text-2xl text-center text-purple-700 font-bold">
+        Hello Tailwind
+      </h1> */}
+      {/* <Typografy /> */}
+    
+      <HoverFocus />
+      {/* <FlexAndResponsive /> */}
+      {/* // <Profile /> */}
+      
+    </div>
+  );
+}
+
+export default App;
